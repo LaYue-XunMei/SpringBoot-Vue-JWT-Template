986e1ef321cc2c964258abeca79eec37cdf8e105
LaYue <2132209089@qq.com>
2025-12-10 16:15:27 +0800

    security配置完成

---
+++ src/main/java/com/example/entity/RestBean.java
@@ -0,0 +1,22 @@
+package com.example.entity;
+
+import com.alibaba.fastjson2.JSONObject;
+import com.alibaba.fastjson2.JSONWriter;
+
+public record RestBean<T>(int code, T data , String message) {//响应码、数据、消息
+    public static <T> RestBean<T> success(T data) {
+        return new RestBean<>(200, data, "请求成功");
+    }
+
+    public static <T> RestBean<T> success() {
+        return success(null);
+    }
+
+    public static <T> RestBean<T> failure(int code, String message) {
+        return new RestBean<>(code, null, message);
+    }
+
+    public String asJsonString() {
+        return JSONObject.toJSONString(this, JSONWriter.Feature.WriteNulls);//Null值不序列化，避免前端报错
+    }
+}
