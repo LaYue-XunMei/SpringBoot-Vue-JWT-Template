47dc9f1e0439a35043c3dce5f307437ea6e47153
LaYue <2132209089@qq.com>
2025-12-12 15:51:00 +0800

    验证码邮件发送完成

--- my-project-frontend/src/main.js
+++ my-project-frontend/src/main.js
@@ -2,11 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from "@/router/index.js";
 import axios from "axios";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
 axios.defaults.baseURL = 'http://localhost:8080'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
--- my-project-frontend/src/router/index.js
+++ my-project-frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import {createRouter, createWebHistory} from "vue-router";
+import {unAuthorized} from "@/net/index.js";
 
 
 const router = createRouter({
@@ -15,9 +16,26 @@ const router = createRouter({
                     component:()=>import("@/views/welcome/LoginPage.vue")
                 }
             ]
+        },
+        {
+            path:"/index",
+            name:"index",
+            component:()=>import("@/views/IndexView.vue")
         }
     ]
 
 })
+//路由守卫
+router.beforeEach((to,from,next)=>{
+    const isUnauthorized = unAuthorized()
+    if(to.name.startsWith('welcome-') && !isUnauthorized){//如果用户已经登录，不能再访问登录页面
+        next('/index')//重定向到index主页
+    }else if(to.fullPath.startsWith('/index') && isUnauthorized){
+        next("/")//r如果没有登录就访问index主页，则重定向到登录页面
+    }else{
+        next()
+    }
+})
+
 
 export default router
